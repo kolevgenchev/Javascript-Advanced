7133f6f2eec0e281f1df746a9565cf8de78e0962
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2023-01-13 12:15:27 +0200

    Add files via upload

---
+++ fruit.js
@@ -0,0 +1,5 @@
+function fruit (fruit,weight,pricePerKg){
+weight=weight/1000
+console.log(`I need $${(weight*pricePerKg).toFixed(2)} to buy ${weight.toFixed(2)} kilograms ${fruit}.`)
+
+}fruit('orange', 2500, 1.80)
