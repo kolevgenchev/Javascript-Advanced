72e053d1b25e4fad37804c131ebc24e4d1cd6c75
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2023-02-23 10:59:22 +0200

    Add files via upload

--- rectangle.js
+++ rectangle.js
@@ -1,17 +1,10 @@
-function rectangle (width,height,color){
-const obj={width,
-    height,
-    color,
-    calcArea(){ return obj.width*obj.height}
+class Rectangle{
+    constructor(width,height,color){
+        this.width=width
+        this.height=height
+        this.color=color
+    }
+    calcArea(){
+        return this.height*this.width
+    }
 }
-if(obj.color[0]!==obj.color[0].toUpperCase()){
-    obj.color=obj.color[0].toUpperCase()+obj.color.slice(1)
-}
-return obj;
-
-}
-let rect = rectangle(4, 5, 'red');
-console.log(rect.width);
-console.log(rect.height);
-console.log(rect.color);
-console.log(rect.calcArea());
