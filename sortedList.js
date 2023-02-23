d51d155c9c870e7767252fa27329ec9e9cb16e0b
Bozhidar Genchev <107246087+kolevgenchev@users.noreply.github.com>
2023-02-23 10:59:05 +0200

    Add files via upload

--- sortedList.js
+++ sortedList.js
@@ -1,26 +1,33 @@
-function createSortedList(){
-    let els=[]
-    
-return{
-    add(element) {
-        els.push(element)
-        els.sort((a,b)=>a-b)
-        
-},
-    remove(index) {if((index >= 0 && index < els.length)){els.splice(index,1)
-        }},
-    get(index) {if((index >= 0 && index < els.length)){return (els[index])}},
-    get size() {
-        return els.length
+class List {
+    list=[]
+    size=0
+    add(x){
+        this.list.push(x)
+        this.size++
+        this.list.sort((a,b)=>a-b)
     }
-}
+    get(index){
+        
+        if(index>this.list.length-1||index<0){
+            throw new Error('index out of bounds')
+        }
+            this.list.sort((a,b)=>a-b)
+            return (this.list[index])
 
+    }
+    remove(index){
+        if(index>this.list.length-1||index<0){
+            throw new Error('index out of bounds')
+        }
+            this.list.sort((a,b)=>a-b)
+            this.list.splice(index,1)
+            this.size--
+
+    }
+    
 }
-let list = createSortedList();
+let list = new List();
 list.add(5);
 list.add(6);
 list.add(7);
-console.log(list.get(1)); 
-list.remove(1);
-console.log(list.get(1));
-console.log(list.size)
+console.log(list.get(82))
